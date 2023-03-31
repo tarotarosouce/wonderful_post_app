246f4265d13bc4e822bc5b866973986a410f6a30
kitaguchi <rv0059ri@kankyousystem.jp>
2023-03-31 22:10:52 +0900

    Articleモデル、articleテーブルの作成 articleテーブルにtitleとcontentの項目を追加

--- db/migrate/20230329005044_create_articles.rb
+++ db/migrate/20230329005044_create_articles.rb
@@ -1,6 +1,6 @@
-class CreateSampleArticles < ActiveRecord::Migration[6.1]
+class CreateArticles < ActiveRecord::Migration[6.1]
   def change
-    create_table :sample_articles do |t|
+    create_table :articles do |t|
       t.string :title, null: false
       t.text :content, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_22_001628) do
+ActiveRecord::Schema.define(version: 2023_03_29_005044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.text "title"
+    t.text "context"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sample_articles", force: :cascade do |t|
     t.string "title"
