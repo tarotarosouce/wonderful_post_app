eca7743057589ad41ac1481a0569fb3c3c5a70d6
kitaguchi <rv0059ri@kankyousystem.jp>
2023-04-05 22:09:15 +0900

    route.rbにresources　:articlesを追記しarticleのルーティングを実装

--- db/migrate/20230322001628_create_sample_articles.rb
+++ db/migrate/20230322001628_create_sample_articles.rb
@@ -1,8 +1,6 @@
 class CreateSampleArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :sample_articles do |t|
-      t.string :title, null: false
-      t.text :content, null: false
 
       t.timestamps
     end
